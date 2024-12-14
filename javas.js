da7dc91aa4259a86ae6c961f271c1a805bef35d7
saxena970 <varshasaxena204@gmail.com>
2024-12-14 17:45:21 +0530

    updatejs

--- javas.js
+++ javas.js
@@ -3,10 +3,5 @@ console.log("Hello World");
 alert("Hello world")
 document.write("<p>This is a paragraph added using <strong>document.write()</strong>.</p>");
 
-const product ={
-    title: "Ball pen",
-    rating: 4,
-    offer: 5,
-    price:270,
-};
-console.log(product);
+
+
